62b88c21097ac032057a3401ddd0e8c05c85e491
Müslüm Barış Korkmazer <11800544+babico@users.noreply.github.com>
2022-08-22 23:20:10 +0300

    while no connection it fill with 0 and epoch time

--- index.js
+++ index.js
@@ -9,31 +9,29 @@ axios
     .get(link)
     .then((response) => {
         let json_text = JSON.stringify(response.data);
-        
-        let json = JSON.parse(                               // edit your starting date as epoch ms
+
+        let json = JSON.parse( // edit your starting date as epoch ms
             '{"date":' + Date.now().toString() + ',"uptime":' + (Date.now() - 1659069369474).toString() + ',' + json_text.slice(1)
         );
 
         if (fs.existsSync(json_loc)) {
-            fs.readFile(json_loc, 'utf8', function(err, data) { 
-                if (err) 
+            fs.readFile(json_loc, 'utf8', function(err, data) {
+                if (err)
                     console.log(err);
 
                 if (data.length == 0) {
                     fs.writeFile(json_loc, '{"data": []}', 'utf8', function(err, data) {
-                        if (err) 
+                        if (err)
                             console.log(err);
                     })
                     toJson(json);
-                }
-                else {
+                } else {
                     toJson(json);
                 }
             });
-        }
-        else {
-            fs.writeFile(json_loc, '{"data": []}', 'utf8', function(err, data) { 
-                if (err) 
+        } else {
+            fs.writeFile(json_loc, '{"data": []}', 'utf8', function(err, data) {
+                if (err)
                     console.log(err);
             })
             toJson(json);
@@ -41,21 +39,63 @@ axios
     })
     .catch((error) => {
         console.log(error);
+        let json = JSON.parse(
+            '{"date":' + Date.now().toString() + `,
+            "uptime":0",
+            "torrents":0,
+            "seeders":0,
+            "completed":0,
+            "leechers":0,
+            "tcp4_connections_handled":0,
+            "tcp4_announces_handled":0,
+            "tcp4_scrapes_handled":0,
+            "tcp6_connections_handled":0,
+            "tcp6_announces_handled":0,
+            "tcp6_scrapes_handled":0,
+            "udp4_connections_handled":0,
+            "udp4_announces_handled":0,
+            "udp4_scrapes_handled":0,
+            "udp6_connections_handled":0,
+            "udp6_announces_handled":0,
+            "udp6_scrapes_handled":0}`
+        );
+        if (fs.existsSync(json_loc)) {
+            fs.readFile(json_loc, 'utf8', function(err, data) {
+                if (err)
+                    console.log(err);
+
+                if (data.length == 0) {
+                    fs.writeFile(json_loc, '{"data": []}', 'utf8', function(err, data) {
+                        if (err)
+                            console.log(err);
+                    })
+                    toJson(json);
+                } else {
+                    toJson(json);
+                }
+            });
+        } else {
+            fs.writeFile(json_loc, '{"data": []}', 'utf8', function(err, data) {
+                if (err)
+                    console.log(err);
+            })
+            toJson(json);
+        }
     });
 
 
 function toJson(obj_) {
-    fs.readFile(json_loc, 'utf8', function(err, data) { 
-        if (err) 
+    fs.readFile(json_loc, 'utf8', function(err, data) {
+        if (err)
             console.log(err);
 
         var obj = JSON.parse(data);
         obj.data.push(obj_);
-    
+
         json = JSON.stringify(obj);
         fs.writeFile(json_loc, json, 'utf8', function(err, data) {
-            if (err) 
-            console.log(err);
+            if (err)
+                console.log(err);
         });
     })
 }
